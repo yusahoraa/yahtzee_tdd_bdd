86e79cf7a3e224a53c9a1a47c01aed371fdb48bf
yusahoraa <felix.ollivier-drolshagen@next-u.fr>
2022-02-20 12:26:15 +0100

    test

--- yahtzee.js
+++
@@ -1,9 +0,0 @@
-function pointsCountOne(num){
-	return num;
-}
-
-function pointsCountTwo(num){
-	return num*2;
-}
-
-module.exports = {pointsCountOne, pointsCountTwo};
--- yahtzee.test.js
+++ yahtzee.test.js
@@ -2,19 +2,7 @@ const yahtzee = require('./yahtzee.js')
 
 
 test('le retour doit être égale à la somme des dés de valeur 1', () => {
-    expect(yahtzee.pointsCountOne(1)).toBe(1);
-    expect(yahtzee.pointsCountOne(3)).toBe(3);
-    expect(yahtzee.pointsCountOne(5)).toBe(5);
-})
-
-test('le retour doit être égale à la somme des dés de valeur 2', () => {
-    expect(yahtzee.pointsCountTwo(1)).toBe(2);
-    expect(yahtzee.pointsCountTwo(3)).toBe(6);
-    expect(yahtzee.pointsCountTwo(5)).toBe(10);
-})
-
-test('le retour doit être égale à la somme des dés de valeur 3', () => {
-    expect(yahtzee.pointsCountThree(1)).toBe(3);
-    expect(yahtzee.pointsCountThree(3)).toBe(9);
-    expect(yahtzee.pointsCountThree(5)).toBe(15);
-})
+    expect(yahtzee.pointsCountOne([1,3,2,5,2])).toBe(1);
+    expect(yahtzee.pointsCountOne([1,1,1,6,3])).toBe(3);
+    expect(yahtzee.pointsCountOne([1,1,1,1,1])).toBe(5);
+});
