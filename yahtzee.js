34969bbbdcb01b88d44bafdb515f3cd5b7478167
yusahoraa <felix.ollivier-drolshagen@next-u.fr>
2022-02-20 16:03:50 +0100

    refactor

--- yahtzee.js
+++ yahtzee.js
@@ -1,73 +1,69 @@
 
-function pointsCountBase(dices, num){
+function pointsCount(dices, type){
 
-	var count = dices.reduce(function(n, val) {
-		return n + (val === num);
-	}, 0);
-
-	var points = count*num;
-
-	return points;
-}
-
-function pointsCountBrelan(dices){
-
-	var count;
 	var points;
-
-	for(var i=1; i <= 6; i++){
-		count = 0;
-		dices.forEach(element => {
-			if(element === i){
-				count ++;
-				if(count === 3){
-					points = count*i;
-				}
-			}
-		});
-	}
-
-	return points;
-}
-
-function pointsCountCarre(dices){
-
 	var count;
-	var points;
 
-	for(var i=1; i <= 6; i++){
-		count = 0;
-		dices.forEach(element => {
-			if(element === i){
-				count ++;
-				if(count === 4){
-					points = count*i;
-				}
+	switch (type) {
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+			count = dices.reduce(function(n, val) {
+				return n + (val === type);
+			}, 0);
+		
+			points = count*type;
+		  	break;
+		case 'Brelan':
+			for(var i=1; i <= 6; i++){
+				count = 0;
+				dices.forEach(element => {
+					if(element === i){
+						count ++;
+						if(count === 3){
+							points = count*i;
+						}
+					}
+				});
 			}
-		});
-	}
+			break;
+		case 'Carre':
+			for(var i=1; i <= 6; i++){
+				count = 0;
+				dices.forEach(element => {
+					if(element === i){
+						count ++;
+						if(count === 4){
+							points = count*i;
+						}
+					}
+				});
+			}
+			break;
+		case 'Quinte':
+			var count;
+			var points;
+		
+			for(var i=1; i <= 6; i++){
+				count = 0;
+				dices.forEach(element => {
+					if(element === i){
+						count ++;
+						if(count ===5){
+							points = 50;
+						}
+					}
+				});
+			}
+		  break;
+		default:
+		  console.log(`Sorry, we are out of ${expr}.`);
+	  }
 
 	return points;
 }
 
-function pointsCountQuinte(dices){
-
-	var count;
-	var points;
-
-	for(var i=1; i <= 6; i++){
-		count = 0;
-		dices.forEach(element => {
-			if(element === i){
-				count ++;
-				if(count ===5){
-					points = 50;
-				}
-			}
-		});
-	}
-
-	return points;
-}
-
-module.exports = {pointsCountBase, pointsCountBrelan, pointsCountCarre, pointsCountQuinte};
+module.exports = {pointsCount};
--- yahtzee.test.js
+++ yahtzee.test.js
@@ -2,55 +2,55 @@ const yahtzee = require('./yahtzee.js')
 
 
 test('le retour doit être égale à la somme des dés de valeur 1', () => {
-    expect(yahtzee.pointsCountBase([1,3,2,5,2], 1)).toBe(1);
-    expect(yahtzee.pointsCountBase([1,1,1,6,3], 1)).toBe(3);
-    expect(yahtzee.pointsCountBase([1,1,1,1,1], 1)).toBe(5);
+    expect(yahtzee.pointsCount([1,3,2,5,2], 1)).toBe(1);
+    expect(yahtzee.pointsCount([1,1,1,6,3], 1)).toBe(3);
+    expect(yahtzee.pointsCount([1,1,1,1,1], 1)).toBe(5);
 });
 
 test('le retour doit être égale à la somme des dés de valeur 2', () => {
-    expect(yahtzee.pointsCountBase([1,3,3,5,2], 2)).toBe(2);
-    expect(yahtzee.pointsCountBase([2,2,2,6,3], 2)).toBe(6);
-    expect(yahtzee.pointsCountBase([2,2,2,2,2], 2)).toBe(10);
+    expect(yahtzee.pointsCount([1,3,3,5,2], 2)).toBe(2);
+    expect(yahtzee.pointsCount([2,2,2,6,3], 2)).toBe(6);
+    expect(yahtzee.pointsCount([2,2,2,2,2], 2)).toBe(10);
 });
 
 test('le retour doit être égale à la somme des dés de valeur 3', () => {
-    expect(yahtzee.pointsCountBase([1,3,2,5,2], 3)).toBe(3);
-    expect(yahtzee.pointsCountBase([2,3,3,6,3], 3)).toBe(9);
-    expect(yahtzee.pointsCountBase([3,3,3,3,3], 3)).toBe(15);
+    expect(yahtzee.pointsCount([1,3,2,5,2], 3)).toBe(3);
+    expect(yahtzee.pointsCount([2,3,3,6,3], 3)).toBe(9);
+    expect(yahtzee.pointsCount([3,3,3,3,3], 3)).toBe(15);
 });
 
 test('le retour doit être égale à la somme des dés de valeur 4', () => {
-    expect(yahtzee.pointsCountBase([1,4,2,5,2], 4)).toBe(4);
-    expect(yahtzee.pointsCountBase([2,4,4,6,4], 4)).toBe(12);
-    expect(yahtzee.pointsCountBase([4,4,4,4,4], 4)).toBe(20);
+    expect(yahtzee.pointsCount([1,4,2,5,2], 4)).toBe(4);
+    expect(yahtzee.pointsCount([2,4,4,6,4], 4)).toBe(12);
+    expect(yahtzee.pointsCount([4,4,4,4,4], 4)).toBe(20);
 });
 
 test('le retour doit être égale à la somme des dés de valeur 5', () => {
-    expect(yahtzee.pointsCountBase([1,3,2,5,2], 5)).toBe(5);
-    expect(yahtzee.pointsCountBase([2,5,5,6,5], 5)).toBe(15);
-    expect(yahtzee.pointsCountBase([5,5,5,5,5], 5)).toBe(25);
+    expect(yahtzee.pointsCount([1,3,2,5,2], 5)).toBe(5);
+    expect(yahtzee.pointsCount([2,5,5,6,5], 5)).toBe(15);
+    expect(yahtzee.pointsCount([5,5,5,5,5], 5)).toBe(25);
 });
 
 test('le retour doit être égale à la somme des dés de valeur 6', () => {
-    expect(yahtzee.pointsCountBase([1,6,2,5,2], 6)).toBe(6);
-    expect(yahtzee.pointsCountBase([2,6,6,6,3], 6)).toBe(18);
-    expect(yahtzee.pointsCountBase([6,6,6,6,6], 6)).toBe(30);
+    expect(yahtzee.pointsCount([1,6,2,5,2], 6)).toBe(6);
+    expect(yahtzee.pointsCount([2,6,6,6,3], 6)).toBe(18);
+    expect(yahtzee.pointsCount([6,6,6,6,6], 6)).toBe(30);
 });
 
 test('le retour doit être égale à la somme des dés du brelan', () => {
-    expect(yahtzee.pointsCountBrelan([1,2,2,5,2])).toBe(6);
-    expect(yahtzee.pointsCountBrelan([2,6,6,6,3])).toBe(18);
-    expect(yahtzee.pointsCountBrelan([5,5,5,5,5])).toBe(15);
+    expect(yahtzee.pointsCount([1,2,2,5,2], 'Brelan')).toBe(6);
+    expect(yahtzee.pointsCount([2,6,6,6,3], 'Brelan')).toBe(18);
+    expect(yahtzee.pointsCount([5,5,5,5,5], 'Brelan')).toBe(15);
 });
 
 test('le retour doit être égale à la somme des dés du carré', () => {
-    expect(yahtzee.pointsCountCarre([1,2,2,2,2])).toBe(8);
-    expect(yahtzee.pointsCountCarre([2,6,6,6,6])).toBe(24);
-    expect(yahtzee.pointsCountCarre([5,5,5,5,5])).toBe(20);
+    expect(yahtzee.pointsCount([1,2,2,2,2], 'Carre')).toBe(8);
+    expect(yahtzee.pointsCount([2,6,6,6,6], 'Carre')).toBe(24);
+    expect(yahtzee.pointsCount([5,5,5,5,5], 'Carre')).toBe(20);
 });
 
 test('le retour doit être égale à 50', () => {
-    expect(yahtzee.pointsCountQuinte([2,2,2,2,2])).toBe(50);
-    expect(yahtzee.pointsCountQuinte([6,6,6,6,6])).toBe(50);
-    expect(yahtzee.pointsCountQuinte([5,5,5,5,5])).toBe(50);
+    expect(yahtzee.pointsCount([2,2,2,2,2], 'Quinte')).toBe(50);
+    expect(yahtzee.pointsCount([6,6,6,6,6], 'Quinte')).toBe(50);
+    expect(yahtzee.pointsCount([5,5,5,5,5], 'Quinte')).toBe(50);
 });
