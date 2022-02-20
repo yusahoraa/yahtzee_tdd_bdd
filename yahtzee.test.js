c7b0437e449fc7759ad9d91593400a8815475341
yusahoraa <felix.ollivier-drolshagen@next-u.fr>
2022-02-20 15:37:09 +0100

    red

--- yahtzee.test.js
+++ yahtzee.test.js
@@ -18,3 +18,27 @@ test('le retour doit être égale à la somme des dés de valeur 3', () => {
     expect(yahtzee.pointsCountBase([2,3,3,6,3], 3)).toBe(9);
     expect(yahtzee.pointsCountBase([3,3,3,3,3], 3)).toBe(15);
 });
+
+test('le retour doit être égale à la somme des dés de valeur 4', () => {
+    expect(yahtzee.pointsCountBase([1,4,2,5,2], 4)).toBe(4);
+    expect(yahtzee.pointsCountBase([2,4,4,6,4], 4)).toBe(12);
+    expect(yahtzee.pointsCountBase([4,4,4,4,4], 4)).toBe(20);
+});
+
+test('le retour doit être égale à la somme des dés de valeur 5', () => {
+    expect(yahtzee.pointsCountBase([1,3,2,5,2], 5)).toBe(5);
+    expect(yahtzee.pointsCountBase([2,5,5,6,5], 5)).toBe(15);
+    expect(yahtzee.pointsCountBase([5,5,5,5,5], 5)).toBe(25);
+});
+
+test('le retour doit être égale à la somme des dés de valeur 6', () => {
+    expect(yahtzee.pointsCountBase([1,6,2,5,2], 6)).toBe(6);
+    expect(yahtzee.pointsCountBase([2,6,6,6,3], 6)).toBe(18);
+    expect(yahtzee.pointsCountBase([6,6,6,6,6], 6)).toBe(30);
+});
+
+test('le retour doit être égale à la somme des dés du brelan', () => {
+    expect(yahtzee.pointsCountBrelan([1,2,2,5,2])).toBe(6);
+    expect(yahtzee.pointsCountBrelan([2,6,6,6,3])).toBe(18);
+    expect(yahtzee.pointsCountBrelan([5,5,5,5,5])).toBe(15);
+});
